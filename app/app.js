f3acc7909691211872031aca6cd2676b93a7430f
Ekin Sokmen <ekin@bidicin.net>
2015-01-06 01:57:23 +0200

    anon function to named function

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 
 angular.module('DecoratedLogDemoApp', [
   'DecoratedLogWithLineNumber'
-]).controller('DecoratedLogDemoCtrl', ['$log', function($log) {
+]).controller('DecoratedLogDemoCtrl', ['$log', function DecoratedLogDemoCtrl($log) {
     $log.log("Test log");
     $log.debug("Test debug");
 }]);
